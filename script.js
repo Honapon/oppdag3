04d621336dab4f719593151a52d6716e17894672
Honapona_ <156892939+Honapon@users.noreply.github.com>
2025-03-18 11:34:35 +0100

    aaa

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var trykke = document.getElementById("knapp");
 var fargeEl = document.getElementById("inputt");
-var bakgrunn = document.getElementsByClassName("kropp")
+
 
 trykke.addEventListener("click", byttFarge);
 
 function byttFarge() {
-  bakgrunn.style.color = fargeEl.value
+  document.body.style.backgroundColor = fargeEl.value
 }
+
+
