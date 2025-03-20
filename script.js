6ecc480466fb906ba9e0766f73b48312bdd9ecb4
Honapona_ <156892939+Honapon@users.noreply.github.com>
2025-03-20 11:52:00 +0100

    aa

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 var trykke = document.getElementById("knapp");
 var fargeEl = document.getElementById("inputt");
-
+var tall = document.getElementById("tall");
+var tekst = document.getElementById("tekst");
+var knp = document.getElementById("knp");
 
 trykke.addEventListener("click", byttFarge);
 
 function byttFarge() {
-  document.body.style.backgroundColor = fargeEl.value
+  var farge = fargeEl.value;
+  var resultat = CSS.supports("color", farge);
+
+  if (resultat == true) {
+    document.body.style.backgroundColor = farge;
+  } else {
+    alert("du er dust!");
+  }
 }
 
+knp.addEventListener("click", ifTest);
 
+function ifTest() {
+  if (tall.value >= 18) {
+    tekst.innerHTML = "du er over 18 år!";
+  } else {
+    tekst.innerHTML = "du er veldig moden for alderen din ;)";
+  }
+}
