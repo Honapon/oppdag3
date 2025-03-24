e931c7d4f094471e6e935906f4f09db2be8b604d
Honapona_ <156892939+Honapon@users.noreply.github.com>
2025-03-24 15:48:45 +0100

    aa

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+//variabler hentet fra HTML dokumentet
 var trykke = document.getElementById("knapp");
 var fargeEl = document.getElementById("inputt");
 var tall = document.getElementById("tall");
 var tekst = document.getElementById("tekst");
 var knp = document.getElementById("knp");
+var valgt = document.getElementById("valg")
+var lagEl = document.getElementById("lagEl")
+var dif = document.getElementById("dif")
+var skrivNoe = document.getElementById("skrivNoe")
 
+//Eventlistenner og funksjon som endrer bakgrunnsfarge
 trykke.addEventListener("click", byttFarge);
 
 function byttFarge() {
@@ -13,16 +19,38 @@ function byttFarge() {
   if (resultat == true) {
     document.body.style.backgroundColor = farge;
   } else {
-    alert("du er dust!");
+    // Ikke vær så frekk da!! Det er intas bra!
+    alert("du er dust! skriv inn en ny farge");
   }
 }
 
-knp.addEventListener("click", ifTest);
+//event listener og funksjon som ser om du 
+//er over eller under 18
+knp.addEventListener("click", ifAlder);
 
-function ifTest() {
+function ifAlder() {
   if (tall.value >= 18) {
-    tekst.innerHTML = "du er over 18 år!";
+    tekst.innerText = "du er over 18 år!";
   } else {
-    tekst.innerHTML = "du er under 18 år!";
+    // Yippie diddy do it!
+    tekst.innerText = "du er under 18 år!";
   }
 }
+
+lagEl.addEventListener("click",nyttEl)
+
+function nyttEl() {
+  var nyEl = valgt.value
+  var nySak = document.createElement(nyEl)
+  
+  if (nyEl == "h1") {
+    nySak.innerText = skrivNoe.value
+    dif.appendChild(nySak)
+
+  } else {
+    nySak.innerText = skrivNoe.value
+    dif.appendChild(nySak)
+  }
+}
+
+
